4e4940dcfa1141e9c1e9b48ca7185d7bf8999d94
Ageha <ageha6912@gmail.com>
2026-09-06 18:30:17 +0800

    chore: 版本 0.3.0 / versionCode 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.japanlearn.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.3.0"
     }
 
     signingConfigs {
